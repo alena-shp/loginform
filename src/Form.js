e748d74ea1ed17ee72eda8b92bdc8110a65d9f5f
Shpakova Alena <shpakovadev@gmail.com>
2019-09-17 15:08:34 +0300

    login form html

---
+++ src/Form.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import logo from './fotoLogin/foto.png'
+
+const Form = () => {
+  return (
+    <div>
+      <h2>
+        <img src={logo} alt="" />
+        Log-in to your account
+      </h2>
+      <form>
+        <div>
+          <i />
+          <input />
+        </div>
+        <div>
+          <i />
+          <input />
+        </div>
+        <button />
+      </form>
+      <div>
+        New to us?
+        <a href="#">Sign Up</a>
+      </div>
+    </div>
+  )
+}
+
+export default Form
